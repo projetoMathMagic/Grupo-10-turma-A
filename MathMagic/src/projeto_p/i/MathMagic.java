5062920c321f40de04eb57a9c82432167661d5fc
João <j.pcarvalho1301@gmail.com>
2023-11-07 20:26:04 -0300

    Att História

--- MathMagic/src/projeto_p/i/MathMagic.java
+++ MathMagic/src/projeto_p/i/MathMagic.java
@@ -7,7 +7,7 @@ package projeto_p.i;
 import java.util.Scanner;
 
 public class MathMagic {
-    
+
     Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -85,9 +85,19 @@ public class MathMagic {
             }
         } while (coletaDados < 1 || coletaDados > 2);
         System.out.println("Soldado: Que Falta de Educação a minha me chamo " + second_name + "!\n"
-                + "Boa Sorte, Bravo " + classe + ", as tropas de apoio se encontram a 5km daqui, tem um grupo de soldados se preparando para ir também, é só seguir a trilha");
+                + "Boa Sorte, Bravo " + classe + ", as tropas de apoio se encontram a 10km daqui, tem um grupo de soldados se preparando para ir também, é só seguir a trilha");
         System.out.println(name + ": Certo");
-        
+        System.out.println("Após quase uma hora indo com seu grupo a direção das tropas, " + name + " houve um som muito alto, se vira e vê o abrigo pegando fogo.");
+        System.out.println("O grupo ficou muito abalado, mas decidiu continuar seu caminho, "+ name +" por outro lado, pensando em sua mãe, decidiu  voltar ao abrigo.");
+        System.out.println("O grupo e " + name +" entraram em discussão, eles não queriam que ele voltasse sozinho, e que continuasse com o grupo.");
+        System.out.println("Porém, ele voltou mesmo assim.");
+        System.out.println("Chegando lá, o exército inimigo já não estava mais lá, só tinha destroços do abrigo.");
+        System.out.println("Após muita procura, ele encontra o corpo de sua mãe.");
+        System.out.println(name + ": Não!!!!\n"
+                + "Não pode ser!\n"
+                + "gritou chorando.");
+        System.out.println("Eu juro que vou me vingar!!");
+
         // Capítulo 2
         System.out.printf("Mathema: %s, maldita insolência, como ousa atacar a minha vila e achar que sairiam impune?", name);
         System.out.printf("%s: prove, velhote! Você e esses seus cabelos brancos não significam NADA, N-A-D-A", name);
@@ -99,7 +109,7 @@ public class MathMagic {
         System.out.println("***********~~~~~|        |~~~~~***********");
         System.out.println("***********|                  |***********");
         System.out.println("****|            lÓGIKI              |****");
-        
+
         System.out.printf("%s: Ixe! E não é que o velhote tava falando sério!", name);
         System.out.println("Mathema: Ué? você não era o bonzão?");
         System.out.printf("%s: Também achava que eu era! Não sou mais, agora me tira daqui!", name);
@@ -114,11 +124,11 @@ public class MathMagic {
         System.out.println("Mathema: \"A potenciação é uma operação matemática."
                 + "Utilizamos a potenciação para indicar multiplicações consecutivas de um mesmo número");
         System.out.printf("%s: Vamos tentar então!", name);
-        
+
         System.out.println("Mathema: Dois elevado a 3 é: ");
         while (true) {
             int resposta = input.nextInt();
-            
+
             if (resposta == 8) {
                 System.out.println("Mathema: Parabéns, você acertou!");
                 break;
@@ -126,11 +136,11 @@ public class MathMagic {
                 System.out.println("Mathema: Tente de novo!");
             }
         }
-        
+
         System.out.println("Mathema: Logo dois elevado a 7 é: ");
         while (true) {
             int resposta = input.nextInt();
-            
+
             if (resposta == 128) {
                 System.out.println("Mathema: Muito bom, você acertou!");
                 break;
@@ -138,27 +148,26 @@ public class MathMagic {
                 System.out.println("Mathema: Tente de novo!");
             }
         }
-        
+
         System.out.println("Mathema: Cinco elevado a 5 é: ");
         while (true) {
             int resposta = input.nextInt();
-            
+
             if (resposta == 3125) {
                 System.out.println("Mathema: Parabéns, você acertou!");
                 break;
             } else {
                 System.out.println("Mathema: Calma! Tente de novo!");
-            }  
+            }
 
-            
         }
-    
+
         System.out.printf("Pegou o jeito da coisa, agora é só continuar, %s. Como recompensa, aqui está um pedaço da escada para você voltar", name);
         System.out.println(""
-                    + "______-------");
-            
+                + "______-------");
+
         System.out.printf("%s: *voz sarcastica* Ah, que lindo, um degrau! Muito obrigado, viu? ~_~", name);
-        
+
     }
 
     public static void creditos() {
