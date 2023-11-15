2c0261b0881a7385c36d432a33ede5b7f61ab3c7
João Pedro Serignolli Borin <143130517+jpserignolli@users.noreply.github.com>
2023-11-15 19:11:01 -0300

    Add files via upload

---
+++ MathMagic/src/projeto_p/i/functions/TemporizadorFalas.java
@@ -0,0 +1,17 @@
+
+package projeto_p.i.functions;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author jpserignolli
+ */
+public class TemporizadorFalas {
+    public static void Falas(String falas, TimeUnit unit, long tempo_mensagem) throws InterruptedException {
+        for (char caractere : falas.toCharArray()) {
+        System.out.print(caractere);
+        unit.sleep(tempo_mensagem);
+        }
+    }   
+}
